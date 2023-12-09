fdcf158cee55203b3eade8b0788768ecf9ec4b07
Alan Ortiz <suza.ortiz4@gmail.com>
2023-12-09 10:44:11 -0700

    Configurando gateway y creando ProductosController.

---
+++ ApiGateway.SQLServer/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using ApiGateway.SQLServer.Models;
+using ApiGateway.SQLServer.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGateway.SQLServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Producto>> ObtenerProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        [HttpGet("{codigo}")]
+        public async Task<Producto?> ObtenerProductosPorCodigo(string codigo)
+        {
+            return await _context.Productos.FirstOrDefaultAsync(x => x.Codigo==codigo);
+        }
+    }
+}
